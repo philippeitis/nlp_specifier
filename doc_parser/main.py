98a4cfbddba2806fb72a6081df5c1a4b01f1ad07
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-08-16 19:28:22 -0700

    Implement first parser pass to read function invocations

--- doc_parser/main.py
+++ doc_parser/main.py
@@ -1,14 +1,17 @@
 from typing import List
 import re
+import logging
 
 import itertools
 from nltk.corpus import wordnet
 
-from pyrs_ast.lib import LitAttr
-from pyrs_ast.scope import Query, FnArg, QueryField
-from pyrs_ast import AstFile
+from pyrs_ast.lib import LitAttr, Fn, HasItems
+from pyrs_ast.scope import Query, FnArg, QueryField, Scope
+from pyrs_ast import AstFile, print_ast
 
-from doc_parser import Parser, Specification
+from doc_parser import Parser, Specification, GRAMMAR_PATH, generate_constructor_from_grammar, is_quote
+from fn_calls import InvocationFactory
+
 
 
 def peek(it):
@@ -97,42 +100,106 @@ class Phrase(QueryField):
         return False
 
 
-def apply_specifications(self, parser: Parser, verbose: bool = False):
-    def vprint(*args):
-        if verbose:
-            print(*args)
-
-    idents = [type_tuple[1] for type_tuple in self.type_tuples()]
-
-    sections = self.docs.sections()
-    vprint([(section.header, section.lines, section.body) for section in self.docs.sections()])
+def apply_specifications(fn: Fn, parser: Parser, scope: Scope, invoke_factory):
+    idents = [type_tuple[1] for type_tuple in fn.type_tuples()]
+    sections = fn.docs.sections()
     for section in sections:
         if section.header is not None:
             continue
-        vprint("SECTION:", section.header, section.sentences)
+        logging.info(f"Specifying documentation section {section.header} of {fn.ident}")
         for sentence in section.sentences:
             try:
-                attr = LitAttr(Specification(next(parser.parse_sentence(sentence, idents=idents))).as_spec())
+                spec = Specification(next(parser.parse_sentence(sentence, idents=idents)), invoke_factory).as_spec()
+                attr = LitAttr(spec)
 
-                vprint("PRINTING SPEC:", attr)
-                self.attrs.append(attr)
+                logging.info(f"[{sentence}] was transformed into the following specification: {attr}")
+                fn.attrs.append(attr)
             except ValueError as v:
-                vprint(f"Unexpected spec {v}")
+                logging.error(f"While specifying [{sentence}], error occurred: {v}")
             except StopIteration as s:
-                vprint(f"Did not find spec for \"{sentence}\"")
+                logging.info(f"No specification could be generated for [{sentence}]")
 
+
+def specify_item(item: HasItems, parser: Parser, scope: Scope, invoke_factory):
+    for sub_item in item.items:
+        if isinstance(sub_item, Fn):
+            apply_specifications(sub_item, parser, scope, invoke_factory)
+        elif isinstance(sub_item, HasItems):
+            specify_item(sub_item, parser, scope, invoke_factory)
+
+
+def find_specifying_sentence(fn: Fn, parser: Parser, invoke_factory: InvocationFactory, word_replacements,
+                             sym_replacements):
+    sections = fn.docs.sections()
+    for section in sections:
+        if section.header is not None:
+            continue
+        logging.info(f"Determining descriptive sentence for {fn.ident}")
+        descriptive_sentence = section.sentences[0]
+        tokens, words = parser.tokenize_sentence(descriptive_sentence)
+        for sym, word in zip(tokens, words):
+            if is_quote(word):
+                continue
+            if word not in word_replacements:
+                word_replacements[word] = set()
+            if sym.value not in sym_replacements:
+                sym_replacements[sym.value] = set()
+            word_replacements[word].add(sym.value)
+            sym_replacements[sym.value].add(word)
+        invoke_factory.add_invocation(fn.ident, descriptive_sentence)
+
+
+def populate_grammar_helper(item: HasItems, parser: Parser, invoke_factory, word_replacements, sym_replacements):
+    for sub_item in item.items:
+        if isinstance(sub_item, Fn):
+            find_specifying_sentence(sub_item, parser, invoke_factory, word_replacements, sym_replacements)
+        elif isinstance(sub_item, HasItems):
+            populate_grammar_helper(sub_item, parser, invoke_factory, word_replacements, sym_replacements)
+
+
+def generate_grammar(ast: AstFile):
+    word_replacements = {}
+    sym_replacements = {}
+    invoke_factory = InvocationFactory(generate_constructor_from_grammar)
+    populate_grammar_helper(ast, Parser.default(), invoke_factory, word_replacements, sym_replacements)
+
+    grammar = invoke_factory.grammar()
+    replacement_grammar = ""
+    for word, syms in word_replacements.items():
+        syms = " | ".join(f"\"{word}_{sym}\"" for sym in syms)
+        grammar = grammar.replace(f"\"{word}\"", f"WD_{word}")
+        replacement_grammar += f"WD_{word} -> {syms}\n"
+    for sym, words in sym_replacements.items():
+        syms = " | ".join(f"\"{word}_{sym}\"" for word in words)
+        grammar = grammar.replace(f"{sym} -> \"{sym}\"", "")
+        replacement_grammar += f"{sym} -> {syms} | \"{sym}\"\n"
+    with open(GRAMMAR_PATH) as f:
+        full_grammar = f.read() + "\n" + grammar + "\n" + replacement_grammar
+
+    return full_grammar, invoke_factory
 
 # TODO: keyword (in doc or in fn name, structural / phrase search, synonyms ok? capitalization ok?
 # TODO: similarity metrics
 
 if __name__ == '__main__':
-    ast = AstFile.from_path("../data/test2.rs")
+    logging.getLogger().setLevel(logging.INFO)
+    ast = AstFile.from_path("../data/test3.rs")
+
+    grammar, invoke_factory = generate_grammar(ast)
+    parser = Parser(grammar)
+
+    # Query Demo
     words = [Word("Hello", False, False), Word("globe", True, False)]
     print("Finding documentation matches.")
     items = ast.scope.find_fn_matches(Query([Phrase(words, parser)]))
     for item in items:
-        print(item)
+        print(item.sig_str())
     print("Finding function argument matches")
     items = ast.scope.find_fn_matches(Query([FnArg(ast.scope.find_type("crate2::Lime"))]))
     for item in items:
-        print(item)
+        print(item.sig_str())
+
+    specify_item(ast, parser, ast.scope, invoke_factory)
+    print_ast(ast)
+
+    print(grammar)
