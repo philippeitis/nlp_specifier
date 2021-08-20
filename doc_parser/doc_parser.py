1c728b2b79e011c42a5d6590167aef82fcdd6ba9
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-08-20 11:22:24 -0700

    Minor refactoring
    This just cleans up some code to be more succinct, renames a function, and adds some documentation.

--- doc_parser/doc_parser.py
+++ doc_parser/doc_parser.py
@@ -1,5 +1,5 @@
-from enum import Enum, auto
-from typing import List, Tuple
+from enum import Enum
+from typing import List, Tuple, Iterator, Iterable
 import logging
 import os
 
@@ -12,7 +12,6 @@ from lemmatizer import lemmatize
 from tokenizer import CodeTokenizer
 
 GRAMMAR_PATH = os.path.join(os.path.dirname(__file__), "codegrammar.cfg")
-
 logger = logging.getLogger("flair")
 logger.setLevel(logging.ERROR)
 
@@ -108,7 +107,7 @@ def apply_operation_tokens(token_dict):
                     set_label(entities[i + 1], "ARITH")
 
 
-def correct_tokens(token_dict, key_words=None):
+def fix_tokens(token_dict, key_words=None):
     def get_label(t) -> str:
         return t["labels"][0].value
 
@@ -168,52 +167,42 @@ def correct_tokens(token_dict, key_words=None):
     apply_operation_tokens(token_dict)
 
 
-def get_leaf_nodes(tree: Tree, nodes: List[Tree]) -> [Tree]:
-    """Returns all leaf nodes, from left to right. Modifies `nodes` in place.
+def replace_leaf_nodes(tree: Tree, values: Iterator[str]):
+    """Replaces leaf nodes in `tree` with words from `values`.
 
     :param tree:
-    :param nodes:
+    :param values:
     :return:
     """
     if isinstance(tree, str):
-        return nodes
+        return
 
     if len(tree) == 1 and isinstance(tree[0], str):
-        nodes.append(tree)
+        tree[0] = next(values)
     else:
         for sub_tree in tree:
-            get_leaf_nodes(sub_tree, nodes)
-
-    return nodes
+            replace_leaf_nodes(sub_tree, values)
 
 
-def attach_words_to_nodes(tree: Tree, words: List[str]) -> Tree:
+def attach_words_to_nodes(tree: Tree, words: Iterable[str]) -> Tree:
     """Modifies `tree` in place, by assigning each word in words to a leaf node in tree, in sequential order.
 
     :param tree:
     :param words:
     :return: modified tree
     """
-    nodes = get_leaf_nodes(tree, [])
-    for node, word in zip(nodes, words):
-        node[0] = word
-
+    replace_leaf_nodes(tree, iter(words))
     return tree
 
 
-class POSModel(Enum):
-    POS = auto()
-    POS_FAST = auto()
-    UPOS = auto()
-    UPOS_FAST = auto()
+class POSModel(str, Enum):
+    POS = "pos"
+    POS_FAST = "pos-fast"
+    UPOS = "upos"
+    UPOS_FAST = "upos-fast"
 
     def __str__(self):
-        return {
-            POSModel.POS: "pos",
-            POSModel.POS_FAST: "pos-fast",
-            POSModel.UPOS: "upos",
-            POSModel.UPOS_FAST: "upos-fast"
-        }[self]
+        return self.value
 
 
 class Parser:
@@ -242,156 +231,161 @@ class Parser:
         self.root_tagger.predict(sentence)
 
         token_dict = sentence.to_dict(tag_type="pos")
-        correct_tokens(token_dict, key_words=idents)
+        fix_tokens(token_dict, key_words=idents)
         labels: List[Label] = [entity["labels"][0] for entity in token_dict["entities"]]
         words = [entity["text"] for entity in token_dict["entities"]]
         return labels, words
 
-    def parse_sentence(self, sentence: str, idents=None, verbose=False) -> Tree:
+    def parse_sentence(self, sentence: str, idents=None, attach_tags=True) -> Tree:
+        """Parses the sentence, using `idents` to detect values in the text.
+        If `idents` is not provided, no IDENT literals will appear in the tree.
+        If `attach_tags` is set, labels will transformed for usage in an augmented CFG.
+        Otherwise, labels will remain unmodified.
+        """
         labels, words = self.tokenize_sentence(sentence, idents)
-        nltk_sent = [label.value if is_quote(word) else f"{word}_{label.value}"
-                     for label, word in zip(labels, words)]
-
-        if verbose:
-            print(labels)
-            print(words)
-            print(nltk_sent)
+        if attach_tags:
+            nltk_sent = [label.value if is_quote(word) else f"{word}_{label.value}"
+                         for label, word in zip(labels, words)]
+        else:
+            nltk_sent = [label.value for label in labels]
 
         for tree in self.rd_parser.parse(nltk_sent):
             yield attach_words_to_nodes(tree, words)
 
 
-# def main():
-#     test_cases = [
-#         ("Returns `true` if `self` is 0u32", "#[ensures(self == 0u32 ==> result)]")
-#     ]
-#     predicates = [
-#         # "I am red",
-#         # "It is red",
-#         # "The index is less than `self.len()`",
-#         # "The index is 0u32",
-#         # Well handled.
-#         # "Returns `true` if and only if `self` is 0u32",
-#         # "Returns `true` if `self` is 0u32",
-#         # "Returns true if self is 0u32",
-#         # # Need to find a corresponding function or method for this case.
-#         # "Returns `true` if and only if `self` is blue",
-#         # # Well handled.
-#         # "Returns `true` if the index is greater than or equal to `self.len()`",
-#         # "Returns `true` if the index is equal to `self.len()`",
-#         # "Returns `true` if the index is not equal to `self.len()`",
-#         # "Returns `true` if the index isn't equal to `self.len()`.",
-#         # "Returns `true` if the index is not less than `self.len()`",
-#         # "Returns `true` if the index is smaller than or equal to `self.len()`",
-#         # "If the index is smaller than or equal to `self.len()`, returns true.",
-#         # "`index` must be less than `self.len()`",
-#         # "for each index `i` from `0` to `self.len()`, `i` must be less than `self.len()`",
-#         # "for each index `i` up to `self.len()`, `i` must be less than `self.len()`",
-#         # "for each index `i` from `0` to `self.len()` inclusive, `i` must be less than `self.len()`",
-#         # "`i` must be less than `self.len()`",
-#         # "for each index `i` from `0` to `self.len()` inclusive, `i` must be less than or equal to `self.len()`",
-#         # "for each index `i` from `0` to `self.len()`, `self.lookup(i)` must not be equal to 0",
-#         # # differentiate btw will and must wrt result
-#         # "for each index `i` from `0` to `self.len()`, `self.lookup(i)` will not be the same as `result.lookup(i)`",
-#         # "For all indices `i` from 0 to 32, `result.lookup(i)` will be equal to `self.lookup(31 - i)`",
-#         # "For all indices `i` between 0 and 32, and less than `amt`, `result.lookup(i)` will be false.",
-#         # # "For all indices `i` between 0 and 32, or less than `amt`, `result.lookup(i)` will be false.",
-#         # "`self.lookup(index)` will be equal to `val`",
-#         # "For all `i` between 0 and 32, and not equal to `index`, `self.lookup(i)` will be unchanged.",
-#         # "For all `i` between 0 and 32, and not equal to `index`, `self.lookup(i)` will not change.",
-#         # "Returns true if self is not blue",
-#         # "`self` must be blue",
-#         # "`other.v` must not be equal to 0",
-#         # "For all `i` between 0 and 32, and not equal to `index`, `self.lookup(i)` will remain static.",
-#         # "Returns `true` if and only if `self == 2^k` for all `k`.",
-#         # "Returns `true` if and only if `self == 2^k` for any `k`."
-#         # "For each index from 0 to 5, `self.lookup(index)` must be true.",
-#         # "For each index up to 5, `self.lookup(index)` must be true.",
-#         # "`a` must be between 0 and `self.len()`.",
-#         # "`a` and `b` must be equal to 0 or `self.len()`.",
-#         # "True is returned.",
-#         # "Returns `a` if `self.check(b)`, otherwise `b`",
-#         # "Returns `a` if `fn(a)`",
-#         # "If `fn(a)`, returns `a`",
-#         # "If `fn(a)`, returns `a`, otherwise `b`",
-#         # "Returns `a` logical and `b`",
-#         # "Returns `a` bitwise and `b`",
-#         # "Returns `a` xor `b`",
-#         # "Returns `a` multiplied by `b`",
-#         # "Returns `a` divided by `b`",
-#         # "Returns `a` subtracted by `b`",
-#         # "Returns `a` subtracted from `b`",
-#         # "Returns `a` added to `b`",
-#         # "Returns `a` shifted to the right by `b`",
-#         # "Returns `a` shifted to the left by `b`",
-#         # "Returns `a` left shift `b`",
-#         # "Returns `a` right shift `b`",
-#         # TODO: Side effects:
-#         #  Assignment operation:
-#         #  Assign result of fn to val
-#         #  Assign result of operation to val
-#         #   eg. Increments a by n
-#         #       Decrements a by n
-#         #       Divides a by n
-#         #       Increases a by n
-#         #       Decreases a by n
-#         #       Negates a
-#         #       Multiplies a by n
-#         #       Subtracts n from a
-#         #       Adds n to a
-#         #       Shifts a to the DIR by n
-#         #       DIR shifts a by n
-#         #       a is shifted to the right by n
-#         #       a is divided by n
-#         #       a is multiplied by n
-#         #       a is increased by n
-#         #       a is incremented by n
-#         #       a is decremented by a
-#         #       a is negated
-#         #       a is right shifted by n
-#         #       a is ?VB?
-#         # "Sets `a` to 1",
-#         # "Assigns 1 to `a`.",
-#         # "Increments `a` by 1",
-#         # "Adds 1 to `a`"
-#         "`a` is incremented by 1",
-#         "`a` is negated",
-#         "`a` is right shifted by `n`",
-#     ]
-#     #         "Returns `true` if and only if `self == 2^k` for some `k`."
-#     #                                                  ^ not as it appears in Rust (programmer error, obviously)
-#     #                                                   (eg. allow mathematical notation?)
-#     #
-#     # TODO: Find examples that are not supported by Prusti
-#     #  async fns (eg. eventually) ? (out of scope)
-#     #  for all / for each
-#     #  Greater than
-#     #  https://doc.rust-lang.org/std/primitive.u32.html#method.checked_next_power_of_two
-#     #  If the next power of two is greater than the type’s maximum value
-#     #  No direct support for existential
-#     #  For any index that meets some condition, x is true. (eg. forall)
-#     parser = Parser.from_path()
-#     for sentence in predicates:
-#         print("=" * 80)
-#         print("Sentence:", sentence)
-#         print("=" * 80)
-#         for tree in parser.parse_sentence(sentence):
-#             tree: Tree = tree
-#             try:
-#                 print(Specification(tree).as_spec())
-#             except LookupError as e:
-#                 print(f"No specification found: {e}")
-#             except UnsupportedSpec as s:
-#                 print(f"Specification element not supported ({s})")
-#             print(tree)
-#             print()
-#
-#     for sentence, expected in test_cases:
-#         try:
-#             tree = next(parser.parse_sentence(sentence))
-#             assert Specification(tree).as_spec() == expected
-#         except AssertionError as a:
-#             print(a)
+def main():
+    test_cases = [
+        ("Returns `true` if `self` is 0u32", "#[ensures(self == 0u32 ==> result)]")
+    ]
+    predicates = [
+        # "I am red",
+        # "It is red",
+        # "The index is less than `self.len()`",
+        # "The index is 0u32",
+        # Well handled.
+        # "Returns `true` if and only if `self` is 0u32",
+        # "Returns `true` if `self` is 0u32",
+        # "Returns true if self is 0u32",
+        # # Need to find a corresponding function or method for this case.
+        # "Returns `true` if and only if `self` is blue",
+        # # Well handled.
+        # "Returns `true` if the index is greater than or equal to `self.len()`",
+        # "Returns `true` if the index is equal to `self.len()`",
+        # "Returns `true` if the index is not equal to `self.len()`",
+        # "Returns `true` if the index isn't equal to `self.len()`.",
+        # "Returns `true` if the index is not less than `self.len()`",
+        # "Returns `true` if the index is smaller than or equal to `self.len()`",
+        # "If the index is smaller than or equal to `self.len()`, returns true.",
+        # "`index` must be less than `self.len()`",
+        # "for each index `i` from `0` to `self.len()`, `i` must be less than `self.len()`",
+        # "for each index `i` up to `self.len()`, `i` must be less than `self.len()`",
+        # "for each index `i` from `0` to `self.len()` inclusive, `i` must be less than `self.len()`",
+        # "`i` must be less than `self.len()`",
+        # "for each index `i` from `0` to `self.len()` inclusive, `i` must be less than or equal to `self.len()`",
+        # "for each index `i` from `0` to `self.len()`, `self.lookup(i)` must not be equal to 0",
+        # # differentiate btw will and must wrt result
+        # "for each index `i` from `0` to `self.len()`, `self.lookup(i)` will not be the same as `result.lookup(i)`",
+        # "For all indices `i` from 0 to 32, `result.lookup(i)` will be equal to `self.lookup(31 - i)`",
+        # "For all indices `i` between 0 and 32, and less than `amt`, `result.lookup(i)` will be false.",
+        # # "For all indices `i` between 0 and 32, or less than `amt`, `result.lookup(i)` will be false.",
+        # "`self.lookup(index)` will be equal to `val`",
+        # "For all `i` between 0 and 32, and not equal to `index`, `self.lookup(i)` will be unchanged.",
+        # "For all `i` between 0 and 32, and not equal to `index`, `self.lookup(i)` will not change.",
+        # "Returns true if self is not blue",
+        # "`self` must be blue",
+        # "`other.v` must not be equal to 0",
+        # "For all `i` between 0 and 32, and not equal to `index`, `self.lookup(i)` will remain static.",
+        # "Returns `true` if and only if `self == 2^k` for all `k`.",
+        # "Returns `true` if and only if `self == 2^k` for any `k`."
+        # "For each index from 0 to 5, `self.lookup(index)` must be true.",
+        # "For each index up to 5, `self.lookup(index)` must be true.",
+        # "`a` must be between 0 and `self.len()`.",
+        # "`a` and `b` must be equal to 0 or `self.len()`.",
+        # "True is returned.",
+        # "Returns `a` if `self.check(b)`, otherwise `b`",
+        # "Returns `a` if `fn(a)`",
+        # "If `fn(a)`, returns `a`",
+        # "If `fn(a)`, returns `a`, otherwise `b`",
+        # "Returns `a` logical and `b`",
+        # "Returns `a` bitwise and `b`",
+        # "Returns `a` xor `b`",
+        # "Returns `a` multiplied by `b`",
+        # "Returns `a` divided by `b`",
+        # "Returns `a` subtracted by `b`",
+        # "Returns `a` subtracted from `b`",
+        # "Returns `a` added to `b`",
+        # "Returns `a` shifted to the right by `b`",
+        # "Returns `a` shifted to the left by `b`",
+        # "Returns `a` left shift `b`",
+        # "Returns `a` right shift `b`",
+        # TODO: Side effects:
+        #  Assignment operation:
+        #  Assign result of fn to val
+        #  Assign result of operation to val
+        #   eg. Increments a by n
+        #       Decrements a by n
+        #       Divides a by n
+        #       Increases a by n
+        #       Decreases a by n
+        #       Negates a
+        #       Multiplies a by n
+        #       Subtracts n from a
+        #       Adds n to a
+        #       Shifts a to the DIR by n
+        #       DIR shifts a by n
+        #       a is shifted to the right by n
+        #       a is divided by n
+        #       a is multiplied by n
+        #       a is increased by n
+        #       a is incremented by n
+        #       a is decremented by a
+        #       a is negated
+        #       a is right shifted by n
+        #       a is ?VB?
+        # "Sets `a` to 1",
+        # "Assigns 1 to `a`.",
+        # "Increments `a` by 1",
+        # "Adds 1 to `a`"
+        "`a` is incremented by 1",
+        "`a` is negated",
+        "`a` is right shifted by `n`",
+    ]
+    #         "Returns `true` if and only if `self == 2^k` for some `k`."
+    #                                                  ^ not as it appears in Rust (programmer error, obviously)
+    #                                                   (eg. allow mathematical notation?)
+    #
+    # TODO: Find examples that are not supported by Prusti
+    #  async fns (eg. eventually) ? (out of scope)
+    #  for all / for each
+    #  Greater than
+    #  https://doc.rust-lang.org/std/primitive.u32.html#method.checked_next_power_of_two
+    #  If the next power of two is greater than the type’s maximum value
+    #  No direct support for existential
+    #  For any index that meets some condition, x is true. (eg. forall)
+    parser = Parser.default()
+    for sentence in predicates:
+        print("=" * 80)
+        print("Sentence:", sentence)
+        print("=" * 80)
+        for tree in parser.parse_sentence(sentence, attach_tags=False):
+            # tree: Tree = tree
+            # try:
+            #     print(Specification(tree).as_spec())
+            # except LookupError as e:
+            #     print(f"No specification found: {e}")
+            # except UnsupportedSpec as s:
+            #     print(f"Specification element not supported ({s})")
+            print(tree)
+            print()
+
+    # for sentence, expected in test_cases:
+    #     try:
+    #         tree = next(parser.parse_sentence(sentence))
+    #         assert Specification(tree).as_spec() == expected
+    #     except AssertionError as a:
+    #         print(a)
 
 
+if __name__ == "__main__":
+    main()
 # confusing examples: log fns, trig fns, pow fns
