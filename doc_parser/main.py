8f27cb24a86de23bed383e6f44017833dc6ee5f6
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-08-21 14:08:57 -0700

    Refactor query code into separate file

--- doc_parser/main.py
+++ doc_parser/main.py
@@ -1,141 +1,16 @@
 from collections import defaultdict
-from typing import List, Collection
-import re
 import logging
-import itertools
 
-import nltk
 from nltk import Tree
-from nltk.corpus import stopwords
 
 from pyrs_ast.lib import LitAttr, Fn, HasItems
-from pyrs_ast.scope import Query, FnArg, QueryField, Scope
+from pyrs_ast.scope import Query, FnArg, Scope
 from pyrs_ast import AstFile
 
 from doc_parser import Parser, GRAMMAR_PATH, is_quote
 from fn_calls import InvocationFactory, Invocation
-from lemmatizer import lemma_eq, is_synonym
 from grammar import Specification, generate_constructor_from_grammar
-
-try:
-    STOPWORDS = set(stopwords.words("english"))
-except LookupError:
-    nltk.download("stopwords")
-    STOPWORDS = set(stopwords.words("english"))
-
-
-def peek(it):
-    first = next(it)
-    return first, itertools.chain([first], it)
-
-
-def tags_similar(tag1: str, tag2: str) -> bool:
-    if tag1 == tag2:
-        return True
-    if tag1.startswith("NN") and tag2.startswith("NN"):
-        return True
-    if tag1.startswith("VB") and tag2.startswith("VB"):
-        return True
-    return False
-
-
-def is_one_of(tag: str, choices: Collection[str]) -> bool:
-    return any(choice in tag for choice in choices)
-
-
-def get_regex_for_tag(tag: str) -> str:
-    if "VB" in tag:
-        return "VB(D|G|N|P|Z)?"
-    if "NN" in tag:
-        return "NN(P|PS|S)?"
-    if "RB" in tag:
-        return "RB(R|S)?"
-    if "JJ" in tag:
-        return "JJ(R|S)?"
-    return tag
-
-
-class Word(QueryField):
-    def __init__(self, word: str, allow_synonyms: bool, is_optional: bool):
-        self.allow_synonyms = allow_synonyms
-        self.word = word
-        self.is_optional = is_optional
-
-    def __str__(self):
-        return self.word
-
-    def matches_fn(self, fn):
-        raise NotImplementedError()
-
-
-class Phrase(QueryField):
-    def __init__(self, phrase: List[Word], parser: Parser):
-        self.parser = parser
-        self.phrase = phrase
-        s = " ".join(word.word for word in phrase)
-        self.pos_tags, _ = parser.tokenize_sentence(s)
-        regex_str = ""
-        for word, tag in zip(self.phrase, self.pos_tags):
-            val = get_regex_for_tag(re.escape(tag.value))
-            if word.is_optional:
-                regex_str += f"({val} )?"
-            else:
-                regex_str += f"{val} "
-
-        regex_str = regex_str[:-1]
-        self.tag_regex = re.compile(regex_str)
-        self.regex_str = regex_str
-
-    def __str__(self):
-        return " ".join(str(x) for x in self.phrase)
-
-    def matches(self, fn: Fn):
-        """Determines whether the phrase matches the provided fn.
-        To match the phrase, the function must contain all non-optional words, in sequence, with no gaps.
-        Words do not need to have matching tenses or forms to be considered equal (using NLTK's lemmatizer).
-        """
-
-        def split_str(s: str, index: int):
-            return s[:index], s[index:]
-
-        docs = fn.docs.sections()
-        if docs:
-            pos_tags, words = self.parser.tokenize_sentence(docs[0].body, idents=[ty.ident for ty in fn.inputs])
-            s = " ".join(tag.value for tag in pos_tags)
-
-            for match in self.tag_regex.finditer(s):
-                prev, curr = split_str(s, match.start(0))
-                curr, after = split_str(curr, match.end(0) - match.start(0))
-
-                match_len = curr.count(" ") + 1
-                match_start = prev.count(" ")
-
-                match_words = words[match_start: match_start + match_len]
-                match_tags = pos_tags[match_start: match_start + match_len]
-                word_iter = iter(zip(match_words, match_tags))
-                matches = True
-                for word, tag in zip(self.phrase, self.pos_tags):
-                    match, word_iter = peek(word_iter)
-                    m_word, m_tag = match
-                    if tags_similar(tag.value, m_tag.value):
-                        next(word_iter)
-                        if lemma_eq(word.word, m_word, tag.value):
-                            continue
-                        elif word.allow_synonyms:
-                            if not is_synonym(word.word, m_word, tag.value):
-                                matches = False
-                                break
-                        else:
-                            matches = False
-                            break
-                    elif word.is_optional:
-                        continue
-                    else:
-                        matches = False
-                        break
-                if matches:
-                    return True
-        return False
+from nlp_query import query_from_sentence, Phrase, Word
 
 
 def tree_references_fn(fn: Fn, tree: Tree) -> bool:
@@ -297,29 +172,6 @@ def generate_grammar(ast, helper_fn=populate_grammar_helper):
     return full_grammar, invoke_factory
 
 
-def query_from_sentence(sentence, parser: Parser) -> Query:
-    """Forms a query from a sentence.
-
-    Stopwords (per Wordnet's stopwords), and words which are not verbs, nouns, adverbs, or adjectives, are all removed.
-    Adverbs and adjectives are optional, and can be substituted with synonyms.
-    """
-    phrases = [[]]
-    pos_tags, words = parser.tokenize_sentence(sentence)
-
-    for pos_tag, word in zip(pos_tags, words):
-        tag = pos_tag.value
-        if word.lower() in STOPWORDS:
-            if phrases[-1]:
-                phrases.append([])
-        elif not is_one_of(tag, {"RB", "VB", "NN", "JJ", "CODE", "LIT"}):
-            if phrases[-1]:
-                phrases.append([])
-        else:
-            is_describer = is_one_of(tag, {"RB", "JJ"})
-            phrases[-1].append(Word(word, allow_synonyms=is_describer, is_optional=is_describer))
-    return Query([Phrase(block, parser) for block in phrases if block])
-
-
 def end_to_end_demo():
     """Demonstrates entire pipeline from end to end."""
     ast = AstFile.from_path("../data/test3.rs")
@@ -469,9 +321,10 @@ def profiling(statement: str):
     cProfile.run(statement, "stats")
     pstats.Stats("stats").sort_stats(pstats.SortKey.TIME).print_stats(20)
 
+
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    end_to_end_demo()
+    search_demo()
 
     # TODO: Detect duplicate invocations.
     # TODO: keyword in fn name, capitalization?
---
+++ doc_parser/nlp_query.py
@@ -0,0 +1,155 @@
+from typing import List, Collection
+import re
+import itertools
+
+import nltk
+from nltk.corpus import stopwords
+
+from pyrs_ast.lib import Fn
+from pyrs_ast.scope import Query, QueryField
+
+from lemmatizer import lemma_eq, is_synonym
+from doc_parser import Parser
+
+try:
+    STOPWORDS = set(stopwords.words("english"))
+except LookupError:
+    nltk.download("stopwords")
+    STOPWORDS = set(stopwords.words("english"))
+
+
+def peek(it):
+    first = next(it)
+    return first, itertools.chain([first], it)
+
+
+def tags_similar(tag1: str, tag2: str) -> bool:
+    if tag1 == tag2:
+        return True
+    if tag1.startswith("NN") and tag2.startswith("NN"):
+        return True
+    if tag1.startswith("VB") and tag2.startswith("VB"):
+        return True
+    return False
+
+
+def is_one_of(tag: str, choices: Collection[str]) -> bool:
+    return any(choice in tag for choice in choices)
+
+
+def get_regex_for_tag(tag: str) -> str:
+    if "VB" in tag:
+        return "VB(D|G|N|P|Z)?"
+    if "NN" in tag:
+        return "NN(P|PS|S)?"
+    if "RB" in tag:
+        return "RB(R|S)?"
+    if "JJ" in tag:
+        return "JJ(R|S)?"
+    return tag
+
+
+class Word(QueryField):
+    def __init__(self, word: str, allow_synonyms: bool, is_optional: bool):
+        self.allow_synonyms = allow_synonyms
+        self.word = word
+        self.is_optional = is_optional
+
+    def __str__(self):
+        return self.word
+
+    def matches_fn(self, fn):
+        raise NotImplementedError()
+
+
+class Phrase(QueryField):
+    def __init__(self, phrase: List[Word], parser: Parser):
+        self.parser = parser
+        self.phrase = phrase
+        s = " ".join(word.word for word in phrase)
+        self.pos_tags, _ = parser.tokenize_sentence(s)
+        regex_str = ""
+        for word, tag in zip(self.phrase, self.pos_tags):
+            val = get_regex_for_tag(re.escape(tag.value))
+            if word.is_optional:
+                regex_str += f"({val} )?"
+            else:
+                regex_str += f"{val} "
+
+        regex_str = regex_str[:-1]
+        self.tag_regex = re.compile(regex_str)
+        self.regex_str = regex_str
+
+    def __str__(self):
+        return " ".join(str(x) for x in self.phrase)
+
+    def matches(self, fn: Fn):
+        """Determines whether the phrase matches the provided fn.
+        To match the phrase, the function must contain all non-optional words, in sequence, with no gaps.
+        Words do not need to have matching tenses or forms to be considered equal (using NLTK's lemmatizer).
+        """
+
+        def split_str(s: str, index: int):
+            return s[:index], s[index:]
+
+        docs = fn.docs.sections()
+        if docs:
+            pos_tags, words = self.parser.tokenize_sentence(docs[0].body, idents=[ty.ident for ty in fn.inputs])
+            s = " ".join(tag.value for tag in pos_tags)
+
+            for match in self.tag_regex.finditer(s):
+                prev, curr = split_str(s, match.start(0))
+                curr, after = split_str(curr, match.end(0) - match.start(0))
+
+                match_len = curr.count(" ") + 1
+                match_start = prev.count(" ")
+
+                match_words = words[match_start: match_start + match_len]
+                match_tags = pos_tags[match_start: match_start + match_len]
+                word_iter = iter(zip(match_words, match_tags))
+                matches = True
+                for word, tag in zip(self.phrase, self.pos_tags):
+                    match, word_iter = peek(word_iter)
+                    m_word, m_tag = match
+                    if tags_similar(tag.value, m_tag.value):
+                        next(word_iter)
+                        if lemma_eq(word.word, m_word, tag.value):
+                            continue
+                        elif word.allow_synonyms:
+                            if not is_synonym(word.word, m_word, tag.value):
+                                matches = False
+                                break
+                        else:
+                            matches = False
+                            break
+                    elif word.is_optional:
+                        continue
+                    else:
+                        matches = False
+                        break
+                if matches:
+                    return True
+        return False
+
+
+def query_from_sentence(sentence, parser: Parser) -> Query:
+    """Forms a query from a sentence.
+
+    Stopwords (per Wordnet's stopwords), and words which are not verbs, nouns, adverbs, or adjectives, are all removed.
+    Adverbs and adjectives are optional, and can be substituted with synonyms.
+    """
+    phrases = [[]]
+    pos_tags, words = parser.tokenize_sentence(sentence)
+
+    for pos_tag, word in zip(pos_tags, words):
+        tag = pos_tag.value
+        if word.lower() in STOPWORDS:
+            if phrases[-1]:
+                phrases.append([])
+        elif not is_one_of(tag, {"RB", "VB", "NN", "JJ", "CODE", "LIT"}):
+            if phrases[-1]:
+                phrases.append([])
+        else:
+            is_describer = is_one_of(tag, {"RB", "JJ"})
+            phrases[-1].append(Word(word, allow_synonyms=is_describer, is_optional=is_describer))
+    return Query([Phrase(block, parser) for block in phrases if block])
