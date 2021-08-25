82cdba500930ee50081920e023e1465928e24eaf
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-08-25 14:29:56 -0700

    Complete parse_html.py
    This should now support all styles of documentation found in stdlib

--- doc_json/parse_html.py
+++ doc_json/parse_html.py
@@ -1,7 +1,6 @@
-import os
-import webbrowser
+import re
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Optional, Union
 
 import bs4
 from bs4 import BeautifulSoup
@@ -10,98 +9,133 @@ SINCE = ("span", {"class": "since"})
 SRCLINK = ("a", {"class": "srclink"})
 
 
-def eat_h1(body) -> str:
-    header = body.find("h1", {"class": "fqn"})
-    to_remove = [SINCE, SRCLINK, ("a", {"href": "javascript:void(0)"})]
-    for item, data in to_remove:
-        for div in header.find_all(item, data):
-            div.decompose()
-    text = str(header.text)
-    header.decompose()
-    return text
+class RList:
+    def __init__(self, doc):
+        self.items = []
+        for item in doc:
+            if isinstance(item, bs4.NavigableString):
+                continue
+            assert item.name == "li"
+            self.items.append(item.text)
 
 
-def parse_example(doc) -> str:
-    for item in doc:
-        if isinstance(item, bs4.NavigableString):
-            continue
-    return "```CODE```"
+class HasDoc:
+    HEADER_RE = re.compile('^h([1-6])$')
+
+    def parse_doc(self, doc):
+        doc_items = []
+        if not doc:
+            return ""
+
+        for item in doc:
+            if isinstance(item, bs4.NavigableString):
+                continue
+            match = self.HEADER_RE.match(item.name)
+            if match and "section-header" in item.attrs["class"]:
+                h = "#" * int(match.group(1))
+                doc_items.append(f"{h} {item.text}")
+            elif item.name == "p":
+                doc_items.append(item.text.replace("\n", " "))
+            elif item.name == "div" and "example-wrap" in item.attrs["class"]:
+                doc_items.append(self.parse_example(item))
+            elif item.name == "ul":
+                doc_items += ["* " + sub_item.strip() for sub_item in RList(item).items]
+            elif item.name == "ol":
+                doc_items += ["{n}. " + sub_item.strip() for n, sub_item in enumerate(RList(item).items)]
+            elif item.name == "blockquote":
+                doc_items += f"> {item.text}"
+            elif item.name == "table":
+                # Tables not handled. std::mem::size_of demonstrates usage.
+                pass
+            elif item.name == "div" and "information" in item.attrs["class"]:
+                # Tooltips are not handled.
+                pass
+            else:
+                print("Unknown item", item)
+
+        doc.decompose()
+        return "/// " + "\n/// ".join(doc_items)
+
+    def parse_example(self, doc) -> str:
+        for item in doc:
+            if isinstance(item, bs4.NavigableString):
+                continue
+        return "```CODE```"
 
 
-def to_rust_doc(doc) -> str:
-    doc_items = []
-    for item in doc:
-        if isinstance(item, bs4.NavigableString):
-            continue
-        if item.name == "p":
-            doc_items.append(item.text)
-        elif item.name == "div" and "example-wrap" in item.attrs["class"]:
-            doc_items.append(parse_example(item))
-        elif item.name == "h1" and "section-header" in item.attrs["class"]:
-            doc_items.append(f"# {item.text}")
-        else:
-            try:
-                if "stab" in item.attrs["class"]:
-                    item.decompose()
-                else:
-                    print(item, item.attrs["class"])
-            except KeyError:
-                print("STABBY")
-                print(item)
-    return "\n".join(doc_items)
+class HasHeader:
+    def __init__(self, body):
+        header = body.find("h1", {"class": "fqn"})
+        to_remove = [SINCE, SRCLINK, ("a", {"href": "javascript:void(0)"})]
+        for item, data in to_remove:
+            for div in header.find_all(item, data):
+                div.decompose()
+        text = str(header.text)
+        header.decompose()
+        self.header = text
 
 
-def eat_struct_doc(body) -> str:
-    items = body.find_all("div", {"class": "docblock"}, recursive=False)
-    type_decl = items[0]
-    doc = items[1]
-    doc_text = to_rust_doc(doc)
-    doc.decompose()
-    type_decl_text = type_decl.text
-    type_decl.decompose()
-    return type_decl_text, doc_text
+class Struct(HasHeader, HasDoc):
+    def __init__(self, body):
+        super().__init__(body)
+        type_decl = body.find("div", {"class": "docblock"})
+        parent = body.find("details", {"class": "rustdoc-toggle"})
+        items = parent.find("div", {"class": "docblock"})
+        doc = items
+        self.doc_text = self.parse_doc(doc)
+        self.type_decl_text = type_decl.text
+        type_decl.decompose()
+
+        self.impls = []
+        for doc in body.find_all("div", {"class": "impl-items"}, recursive=False):
+            self.eat_impl(doc)
+
+    def eat_impl(self, doc):
+        items = []
+        for item in doc:
+            if item.name == "h4":
+                for target_type in [
+                    SINCE, SRCLINK,
+                    ("a", {"href": "javascript:void(0)"}),
+                ]:
+                    for target in item.find_all(*target_type):
+                        target.decompose()
+                items.append(("METHOD", item.text))
+                item.decompose()
+            if item.name == "div":
+                items.append(("DOC", self.parse_doc(item)))
+        return items
+
+    def decl(self):
+        return self.doc_text + "\n" + self.type_decl_text
 
 
-def eat_fn_doc(body) -> str:
-    doc = body.find("div", {"class": "docblock"}, recursive=False)
-    if not doc:
+class Fn(HasHeader, HasDoc):
+    def __init__(self, body):
+        super().__init__(body)
+        fn_decl_block = body.find("pre", {"class": "fn"})
+        for span in fn_decl_block.find_all("span"):
+            span.decompose()
+        self.fn_decl = fn_decl_block.text
         parent = body.find("details", {"class": "rustdoc-toggle"})
         if not parent:
-            return ""
+            self.doc_text = ""
+            return
         doc = parent.find("div", {"class": "docblock"}, recursive=False)
+        self.doc_text = self.parse_doc(doc)
 
-    doc_text = to_rust_doc(doc)
-    doc.decompose()
-    return doc_text
-
-
-def eat_impl_item(body) -> list:
-    items = []
-    for item in body:
-        if item.name == "h4":
-            for target_type in [
-                SINCE, SRCLINK,
-                ("a", {"href": "javascript:void(0)"}),
-            ]:
-                for target in item.find_all(*target_type):
-                    target.decompose()
-            items.append(("METHOD", item.text))
-            item.decompose()
-        if item.name == "div":
-            items.append(("DOC", to_rust_doc(item)))
-            item.decompose()
-    return items
+    def decl(self):
+        return self.doc_text + "\n" + self.fn_decl + " {}"
 
 
-def eat_impls(body) -> list:
-    items = []
-    for doc in body.find_all("div", {"class": "impl-items"}, recursive=False):
-        items.append(eat_impl_item(doc))
-    return items
+DISPATCH = {
+    "fn": Fn,
+    "struct": Struct,
+}
 
 
 def get_all_doc_files(path: Path) -> list:
-    if path.is_dir() and path.name in ["rust-by-example",  "reference", "embedded-book", "edition-guide"]:
+    if path.is_dir() and path.name in ["rust-by-example", "reference", "embedded-book", "edition-guide"]:
         return []
 
     choices = ["struct.", "fn.", "enum.", "constant.", "macro.", "trait."]
@@ -112,7 +146,7 @@ def get_all_doc_files(path: Path) -> list:
             items += get_all_doc_files(child_path)
         else:
             name = child_path.name
-            if any(name.startswith(choice) for choice in choices) and not "arch" in str(path):
+            if any(name.startswith(choice) for choice in choices) and "arch" not in str(path):
                 items.append(child_path)
             # can also have trait or macro
     return items
@@ -132,39 +166,43 @@ def files_into_dict(paths: List[Path]) -> Dict[str, List[Path]]:
     return path_dict
 
 
-def parse_file(path: Path):
+def parse_file(path: Path) -> Optional[Union[Struct, Fn]]:
     with open(path, "r") as file:
-        file_name = os.path.basename(path)
-        print(file_name)
         soup = BeautifulSoup(file.read(), 'html.parser')
         if soup.find("title").text == "Redirection":
             return
         body = soup.find("section")
-        print(eat_h1(body))
-        if file_name.split(".", 1)[0] == "struct":
-            print(eat_struct_doc(body))
-            print("\n".join(str(x) for x in eat_impls(body)))
-        elif file_name.split(".", 1)[0] == "fn":
-            print(eat_fn_doc(body))
+        return DISPATCH.get(
+            #
+            path.stem.split(".", 1)[0],
+            # Default case: return None
+            lambda *args, **kwargs: None
+        )(body)
 
 
 def main():
     TOOLCHAIN_ROOT = "~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/"
-    print(os.environ.get("RUSTUP_HOME"))
-    # path = os.path.expanduser(HTML_ROOT + "/rust/html/core/num/struct.NonZeroI8.html")
     target_dir = (Path(TOOLCHAIN_ROOT) / Path("share/doc/rust/html/")).expanduser()
-
-    # path = os.path.expanduser(TOOLCHAIN_ROOT + "share/doc/rust/html/core/char/fn.from_u32.html")
     # webbrowser.open(path)
 
     files = files_into_dict(get_all_doc_files(target_dir))
     counts = {
         path_type: len(items) for path_type, items in files.items()
     }
-    for file in files["fn"]:
-        parse_file(file)
 
-    exit()
+    sucesses = []
+    for path in files["fn"] + files["struct"]:
+        file = parse_file(path)
+        print(path)
+        if file:
+            sucesses.append((path, file))
+
+    for path, file in sucesses:
+        file_name = path.name
+        # print(file_name)
+        # print(file.decl())
+
+    print(len(sucesses), counts)
 
 
 if __name__ == '__main__':
