08ef56e4363c9fcb5c7ca734420fceddb77a7448
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-10-01 15:14:12 -0700

    Specify what dependencies are used for

--- src/setup.sh
+++ src/setup.sh
@@ -1,4 +1,4 @@
-sudo apt install python3-dev python-dev graphviz
+sudo apt install python3-dev graphviz
 pip install -U pip setuptools wheel
 pip install -U spacy[cuda112]
 python -m spacy download en_core_web_lg
