59e342b2a42a47b49c7883a5ddd8a5e9fde53608
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-10-06 17:46:17 -0700

    Make a note of strengths and weaknesses of various models

--- src/nlp/similarity.py
+++ src/nlp/similarity.py
@@ -1,9 +1,24 @@
 from tokenizer import Tokenizer, SpacyModel
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+class Space:
+    def __init__(self, width: int):
+        self.width = width
+
+    def __str__(self):
+        return " " * self.width
 
 
 class NaiveSimilarity:
+    """
+    Uses the word-vector cosine similarity metric. In practice, is quite fast,
+    but does not detect reordering of words.
+
+    Additionally, words like "maximum" and "minimum" tend to have similar
+    classifications (though more synonymous words will be more similar)
+    """
     def __init__(self, tokenizer: Tokenizer):
         self.tokenizer = tokenizer
 
@@ -14,6 +29,11 @@ class NaiveSimilarity:
 
 
 class SimilarityFilter:
+    """
+    Uses the word-vector cosine similarity metric, applying some filter over the individual
+    tokens (words). Refer to NaiveSimilarity docs for information on strengths and weaknesses
+    """
+
     def __init__(self, tokenizer: Tokenizer, filter_fn):
         self.tokenizer = tokenizer
         self.filter_fn = filter_fn
@@ -30,16 +50,31 @@ class SimilarityFilter:
 
 
 class SimilarityNoStop(SimilarityFilter):
+    """
+    Filters out all stop words from the sentence and computes the word-vector cosine similarity metric
+    on the resulting sentence.
+    """
+
     def __init__(self, tokenizer: Tokenizer):
         super().__init__(tokenizer, lambda t: not t.is_stop)
 
 
 class SimilarityNouns(SimilarityFilter):
+    """
+    Filters out all words which are not nouns or verbs from the sentence and computes the word-vector cosine similarity
+     metric on the resulting sentence.
+    """
     def __init__(self, tokenizer: Tokenizer):
         super().__init__(tokenizer, lambda t: t.pos_ in {'NOUN', 'PROPN', "VERB"})
 
 
 class SimilarityBert:
+    """
+    Computes similarity using BERT, which is sensitive to the context in which words appear.
+
+    Is very effective at detecting dissimilar sentences, but underperforms when measuring if sentences
+    are similar.
+    """
     def __init__(self):
         # Takes a long time to load these models
         model_name = "bert-base-cased-finetuned-mrpc"
@@ -70,11 +105,11 @@ def similarity_metrics(sentence_pairs):
 
     for sent1, sent2 in sentence_pairs:
         print("=" * 80)
-        print(f"    S1: {sent1}")
-        print(f"    S2: {sent2}")
+        print(f"{Space(4)}S1: {sent1}")
+        print(f"{Space(4)}S2: {sent2}")
 
         for name, sim_metric in sims:
-            print(f"        {name}: {sim_metric(sent1, sent2)}")
+            print(f"{Space(8)}{name}: {sim_metric(sent1, sent2)}")
 
 
 # TODO: Expose this in a server API
@@ -91,7 +126,8 @@ if __name__ == '__main__':
         ("Deletes the last element of self.", "Removes the last element of self."),
         # Tests similarity metrics on different sentence
         # In BERT, this is very dissimilar, but by default, quite similar
-        ("Delete the last element of self.", "Returns the maximum of two `f32` values.")
+        ("Delete the last element of self.", "Returns the maximum of two `f32` values."),
+        ("The last element of self.", "self the last element of.")
     ]
 
     similarity_metrics(sentence_pairs)
