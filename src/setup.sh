ca7e264c9e0810282b8c8110b28362ec2288322c
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-10-29 20:57:01 -0700

    Remove visualization from setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -1,7 +1,6 @@
 sudo apt install graphviz
 pip install -U pip setuptools wheel
 pip install -U stanza
-pip install -r ./visualization/requirements.txt
 pip install -r ./codegen/requirements.txt
 pip install -r ./nlp/requirements.txt
 python -m spacy download en_core_web_lg
