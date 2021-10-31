943466b8fa5f588dd8fc9bce51c509d209602a76
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-10-31 01:45:09 -0700

    Don't immediately render tokenization on page load

--- src/webui/__init__.py
+++ src/webui/__init__.py
@@ -6,28 +6,40 @@ import streamlit as st
 SERVICE_URL = os.getenv("NLP_SERVICE_URL", "http://0.0.0.0:5000")
 st.title("")
 
-sentence = st.text_input("Text to tokenize", "Returns true if and only if `x == 2`")
-methods = {
-    "Parts of speech": "pos",
-    "Dependencies": "deps",
-    "Named entities": "ner",
-    "Semantic role labels": "srl",
-}
 
-method = methods[st.selectbox("Render", methods)]
-if method in {"pos", "deps"}:
-    retokenize = st.checkbox("Apply retokenization", value=True)
-    params = {"sentence": sentence, "retokenize": retokenize}
-else:
-    params = {"sentence": sentence}
+def render_items(method, params):
+    body = requests.get(
+        f"{SERVICE_URL}/render/{method}",
+        params=params,
+    ).text
+
+    body = "\n".join(line for line in body.split("\n") if line.strip())
+    style = "<style>mark.entity { display: inline-block }</style>"
+
+    st.write(f"{style}{body}", unsafe_allow_html=True)
 
 
-body = requests.get(
-    f"{SERVICE_URL}/render/{method}",
-    params=params,
-).text
+def read_tokenization_params():
+    sentence = st.text_input("Text to tokenize", "Returns true if and only if `x == 2`")
 
-body = "\n".join(line for line in body.split("\n") if line.strip())
-style = "<style>mark.entity { display: inline-block }</style>"
+    methods = {
+        "Parts of speech": "pos",
+        "Dependencies": "deps",
+        "Named entities": "ner",
+        "Semantic role labels": "srl",
+    }
 
-st.write(f"{style}{body}", unsafe_allow_html=True)
+    method = methods[st.selectbox("Render", methods)]
+
+    if method in {"pos", "deps"}:
+        retokenize = st.checkbox("Apply retokenization", value=True)
+        params = {"sentence": sentence, "retokenize": retokenize}
+    else:
+        params = {"sentence": sentence}
+
+    return st.button("Submit"), method, params
+
+
+submit, method, params = read_tokenization_params()
+if submit:
+    render_items(method, params)
