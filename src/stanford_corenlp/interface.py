b014d64dc91b96961c30d5e695520c2f5a5855bc
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-11-08 17:54:28 -0800

    Add Tree helper for stanza

--- src/stanford_corenlp/interface.py
+++ src/stanford_corenlp/interface.py
@@ -1,8 +1,23 @@
 import stanza
 from stanza.server import CoreNLPClient
 
+
+class Tree:
+    def __init__(self, parse_tree):
+        self.value = parse_tree.value
+        self.score = parse_tree.score
+        self.children = [Tree(child) for child in parse_tree.child]
+
+    def __str__(self):
+        if self.children:
+            return f"({self.value} {' '.join(str(child) for child in self.children)})"
+        return f"{self.value}"
+
 if __name__ == '__main__':
     stanza.install_corenlp()
+    stanza.download()
+    nlp = stanza.Pipeline(lang='en', processors='tokenize,pos,constituency')
+
     with CoreNLPClient(
             annotators=['tokenize', 'ssplit', 'pos', 'lemma', 'ner', 'parse', 'depparse', 'coref'],
             timeout=30000,
@@ -13,5 +28,5 @@ if __name__ == '__main__':
             "He does something when x is true",
         ]:
             ann = client.annotate(line)
-            print(ann.sentence[0].parseTree)
-            print(ann.sentence[0].parseTree.child)
+            print(Tree(ann.sentence[0].parseTree.child[0]))
+            print(nlp(line).sentences[0].constituency)
